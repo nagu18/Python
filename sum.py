1d45853539ce2fda8c38a5234f547837d0da6f96
nagendher <kondanagendher8@gmail.com>
2025-03-07 02:11:24 +0530

    simple basic

---
+++ sum.py
@@ -0,0 +1,7 @@
+def sum(a,b,c,):
+    return a +b + c   #funtion in python add def is to declare  
+a = int(input())       # the funtion variable = <data-type>(input()) 
+b = int(input())
+c = int(input())
+print(sum(a,b,c))
+
