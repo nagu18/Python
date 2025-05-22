89f424017157543d02b6d3ad9b2262ba34903852
Nagu <kondanagendher8@gmail.com>
2025-05-22 08:27:46 +0530

    update

--- sum.py
+++
@@ -1,7 +0,0 @@
-def sum(a,b,c,):
-    return a +b + c   #funtion in python add def is to declare  
-a = int(input())       # the funtion variable = <data-type>(input()) 
-b = int(input())
-c = int(input())
-print(sum(a,b,c))
-
---
+++ variable.py
@@ -0,0 +1,4 @@
+message = "hello nagu"
+print(message)
+message = "let's go"
+print (message)
