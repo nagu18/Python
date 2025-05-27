a7bcaa43b0b3b6d1690ffe5401d93dca194707d4
Nagu <kondanagendher8@gmail.com>
2025-05-28 02:34:57 +0530

    upadate

--- Strings/WhiteSpace/stripping_whitespace.py
+++ Strings/WhiteSpace/stripping_whitespace.py
@@ -1,8 +1,9 @@
-var1 = "nagu bhai"
-var1 = var1.rsplit()
+var1 = " nagu " # as we can see here with  space right 
+var1 = var1.rstrip() #remove the right-side space 
 print(var1)
-# has we can see that the  space right or left of string is consider
-# individual String with no white space basically it remove the white space 
+# output nagu without space  
+# has we can see that the  space right of string is consider
+# individual String with no white space basically it remove the white space on right-side 
 # used Scenario login's right where the user need to enter a username so when he enter 
 # user name so example user name is nagu orginal , so when i enter nagu with a space 
 #                                                                       ^
@@ -10,4 +11,7 @@ print(var1)
 # with out the rsplit it can very nagu is no equal  (nagu with space)
 #                                                        ^
 #                                                   here the space 
+ # lstrip it will remove at left ---------------------------------------------------------
+var1 = var1.lstrip()
+print (var1)
 print("read the code to understand clearly")
