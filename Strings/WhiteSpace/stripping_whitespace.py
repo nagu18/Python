289b244cf9352278c48c221c27295964e28fbca1
Nagu <kondanagendher8@gmail.com>
2025-05-28 02:05:49 +0530

    update

---
+++ Strings/WhiteSpace/stripping_whitespace.py
@@ -0,0 +1,13 @@
+var1 = "nagu bhai"
+var1 = var1.rsplit()
+print(var1)
+# has we can see that the  space right or left of string is consider
+# individual String with no white space basically it remove the white space 
+# used Scenario login's right where the user need to enter a username so when he enter 
+# user name so example user name is nagu orginal , so when i enter nagu with a space 
+#                                                                       ^
+#                                                                   here the space 
+# with out the rsplit it can very nagu is no equal  (nagu with space)
+#                                                        ^
+#                                                   here the space 
+print("read the code to understand clearly")
