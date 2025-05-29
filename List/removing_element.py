fad3558a876ee3fe1daca56990d8fac2cb78e3b4
Nagu <kondanagendher8@gmail.com>
2025-05-30 01:57:54 +0530

    list-update

---
+++ List/removing_element.py
@@ -0,0 +1,5 @@
+games_i_like= ["bgmi", "lost of us", "lost of us 2", "gta5", "templerun"]
+print(f"\n{games_i_like}") 
+del games_i_like[4] # to delete the element 
+print(f"\n{games_i_like}")
+print("\nread the code ")
