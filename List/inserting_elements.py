39977eee49529356c6b958839ddb2228bfb287cf
Nagu <kondanagendher8@gmail.com>
2025-05-31 02:04:20 +0530

    upadte

--- List/inserting_elements.py
+++ List/inserting_elements.py
@@ -1,5 +1,6 @@
 games = ["pubg", "lost of us", "gta5", "lost of us 2"]
-# insering in a index position where the old element is replaced with new one
+# insering in a index position where the  old element position is replaced with new one
 games.insert(0, "bgmi") 
 print(games)
 print("\nread the code")
+# as u can see here bgmi got into postion 0 and pubg into 1 
