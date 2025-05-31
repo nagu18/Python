f88815effee23362850a2f267c8900a124ab86bf
Nagu <kondanagendher8@gmail.com>
2025-05-31 22:47:21 +0530

    update

---
+++ Exercise/3_7_shrinking_guest_list.py
@@ -0,0 +1,33 @@
+ppl_invite = []
+ppl_invite.append('sabitha')
+ppl_invite.append('vijaya')
+ppl_invite.append('shekar')
+ppl_invite.append('narasama')
+ppl_invite.append('venkat')
+ppl_invite.append('roja')
+ppl_invite.append('narasaya')
+print(f"\nhey all : {ppl_invite}")
+# using pop 
+ppl_2ppl =['nana','tana','goma','lama']
+print(f"\nso two sit's left than i can only to ppl guys\n")
+p1=ppl_2ppl.pop(3)
+p2=ppl_2ppl.pop(2)
+#has we can only call two ppl 
+ppl_invite.append(p1)
+ppl_invite.append(p2)
+print(f"hey welcome our new comer's to the party {p1},{p2}\n")
+print(f"new list:\n{ppl_invite}\ni am sorry about {ppl_2ppl} i will plan a 3oo party with them ")
+#for del 
+del ppl_invite[1] 
+del ppl_invite[0]
+print("bye bye go safely sabitha and vijaya ")
+print(f"ppl how are left\n{ppl_invite}")
+del ppl_invite[0]
+del ppl_invite[0]
+del ppl_invite[0]
+del ppl_invite[0]
+del ppl_invite[0]
+del ppl_invite[0]
+del ppl_invite[0]
+
+print (f"bye bye all:- \n{ppl_invite}")
