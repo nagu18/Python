b377b160188d37dada5e7e059c614d0d3a773179
Nagu <kondanagendher8@gmail.com>
2025-06-04 01:31:28 +0530

    update

---
+++ List/index_error.py
@@ -0,0 +1,9 @@
+names=['nagu','konda','nagi']
+print(names[3])
+#it throw an index error 
+games=[]
+print(games[-1])
+
+##if the want to print an element, is not there 
+##in the list it throw an index error
+
