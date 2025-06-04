28d3b090e893cd8d807c66ecd80d2b4cfd5e402f
Nagu <kondanagendher8@gmail.com>
2025-06-05 02:05:50 +0530

    update

---
+++ List/looping_list.py
@@ -0,0 +1,4 @@
+games=['bgmi', 'resident evil', 'lost of us', 'lost of us2']
+for game in games:
+    print(game)
+# to print the list elements all at onces by reading the games and set the variable and used for printing
