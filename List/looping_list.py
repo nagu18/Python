8203c2e404d0909d2f79048f65c6f64d27942461
Nagu <kondanagendher8@gmail.com>
2025-06-05 02:20:37 +0530

    upload

--- List/looping_list.py
+++ List/looping_list.py
@@ -2,3 +2,11 @@ games=['bgmi', 'resident evil', 'lost of us', 'lost of us2']
 for game in games:
     print(game)
 # to print the list elements all at onces by reading the games and set the variable and used for printing
+# make sure that seting a singular variable so u can Understood well and all as well 
+#-------------------|
+#for cat in cats    |
+#    print(cat)     |
+#for list in lists  |
+#     print(list)   |
+#-------------------|
+print("\nread the code")
