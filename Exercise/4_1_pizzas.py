9aaf8198022d3d8442db0d5ccd386f3083dfe8c9
nagu18 <kondanagendher8@gmail.com>
2025-06-14 19:36:26 +0530

    update

---
+++ Exercise/4_1_pizzas.py
@@ -0,0 +1,5 @@
+pizza = ['chikken','veggie','hawaiian']
+for  x in pizza:
+    print(f"i love this {x} pizza")
+
+print("\ni really love pizzas i eat daily a pizza")
---
+++ Exercise/4_2_annimals.py
@@ -0,0 +1,5 @@
+annimal = ['dog', 'cat', 'got', 'fox']
+for x in annimal:
+    print(f"this are the annimals u can  choose {x}")
+
+print('\nany of this annimal be a greate choose for u')
