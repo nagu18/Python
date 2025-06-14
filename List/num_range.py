436f81e50d2c669677e863b383c91533a15e03d4
nagu18 <kondanagendher8@gmail.com>
2025-06-14 20:08:13 +0530

    update

--- List/num_range.py
+++ List/num_range.py
@@ -1,5 +1,6 @@
 for num in range(1, 5):
     print(num)
 
+# range() is the function 
 # range is the keyword --range(1, 5)--
 # the second value we provide will it lesser vlaue due to it count for 0 
