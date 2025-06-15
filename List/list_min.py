e8397f6e771280b266578438cfa9323e0d14633d
Nagu <kondanagendher8@gmail.com>
2025-06-15 22:26:53 +0530

    list-sum

---
+++ List/list_min.py
@@ -0,0 +1,12 @@
+# to find the minimum digits in a list 
+mini = []
+mini.append(1)
+mini.append(2)
+mini.append(3)
+mini.append(0)
+mini.append(11)
+mini.append(12)
+mini.reverse()
+print(mini)
+
+print(f"to find the minimun num in the list:\t{min(mini)}")
---
+++ List/list_sum.py
@@ -0,0 +1,4 @@
+add = [1,2,3,4,5,6,7,8,9,10]
+print(f"sum of the number: {sum(add)}")
+#sum is the function to add element in list 
+
