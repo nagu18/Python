3c15726bab9c809f2e4ce36bf50184878f491c73
Nagu <kondanagendher8@gmail.com>
2025-06-16 11:24:03 +0530

    update

--- Exercise/4-_4_printing_one_milion_list_range.py
+++
@@ -1,8 +0,0 @@
-#count_million=[]
-#for count in range(1,1_000_001):
-#    count_million.append(count)
-#print(count_million)
-
-#using list function 
-count=list(range(1,1000_001))
-print(count)
---
+++ Exercise/4_4_printing_one_milion_list_range.py
@@ -0,0 +1,8 @@
+count_million=[]
+for count in range(1,1_000_001):
+    count_million.append(count)
+print(count_million)
+
+#using list function (simple-way):-
+count=list(range(1,1000_001))
+print(count)
