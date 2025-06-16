651ebde251fbf1c02c455b0e66a4f7a7f9922ae9
Nagu <kondanagendher8@gmail.com>
2025-06-16 11:04:17 +0530

    update

---
+++ List/list_squre_short_comperhensive_simpelway.py
@@ -0,0 +1,16 @@
+sq =[value**2 for value in range(1,11)]
+print(sq)
+# so we squre the element's in single line so we need to delcare the value**2 in list for loop as well.
+
+# clear writeing -- squre_append_rage.py
+cube=[]
+for v in range(1,11):
+    c = v**3
+    cube.append(c)
+
+print(f"\nthis is cube:  {cube}")
+# some what simple
+qurd = []
+for qv in range(1,11):
+    qurd.append(qv**4)
+print(f"\nthis is qurd:  {qurd}")
