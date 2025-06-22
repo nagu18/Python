b9b492a8002bf396c78b2c4301c767a31f2d6200
Nagu <kondanagendher8@gmail.com>
2025-06-22 06:52:01 +0530

    update

---
+++ Exercise/5_10_cheking_usernames.py
@@ -0,0 +1,15 @@
+current_user = ['nagu18','konda@2004','nagu2004','nagendar2004','Konda']
+newuser = ['Nagu18','kOnda@2004','love','amulya']
+lower_current_user = [user.lower() for user in current_user]
+for user in newuser:
+    if user.lower() in lower_current_user:
+        print(f'user {user} exit ')
+    elif (user not in current_user):
+        current_user.append(user.lower())        
+    
+print(current_user)
+
+
+
+
+
---
+++ Exercise/5_11_ordinal_numbers.py
@@ -0,0 +1,21 @@
+nums = list(range(1,10))
+for num in nums:
+    if num == 1:
+        print("1st")
+    elif num == 2:
+        print("2nd")
+    elif num == 3:
+        print("3rd")
+    elif num == 4:
+        print("4th")
+    elif num == 5:
+        print("5th")
+    elif num == 6:
+        print("6th")
+    elif num == 7:
+        print("7th")
+    elif num == 8:
+        print("8th")
+    elif num == 9:
+        print("9th")
+        
