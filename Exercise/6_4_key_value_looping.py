dffff21cc717a093371d99e3e3c35938aa556bae
Nagu <kondanagendher8@gmail.com>
2025-06-27 14:55:13 +0530

    update

---
+++ Exercise/6_4_key_value_looping.py
@@ -0,0 +1,22 @@
+my_python_knowlage={'lstrip':'remove left side whitespace',
+                    'rstrip':'remove right side whitespace',
+                    'strip':'remove whitspace of word',
+                    'remove.prefix()':'to romove prefix of any string',
+                    'remove.suffix':'to remove suffix string',
+                    'append()':'to add element in list',
+                    'insert()':'to insert an element in a postion x-e y-p',
+                    'pop()':'to del a element a store it in a variable pop(position)',
+                    'remove()':'to remove an elememt by it call it in remove(element)',
+                    'del lisr[element]': 'it delete the element perminet',
+                    'extend.()':'to add two list element',
+                    'list[postion]=that u want to modify':'used to  modify an element',
+                    'len()':"to know the lenght of the list any thing like string",
+                    'max()':'it tell the max element in list',
+                    'min':'it the min element in list',
+                    'add':'add the element in list',
+                    'sort()':'to sord by alphabets reverse=True for revers alha sort',
+                    'print(listname.sorted())':'for tem sort',
+                    'reverse.()':'to sort in reverse'
+                    }
+for key,value in my_python_knowlage.items():
+    print({f'{key}:{value}'})
---
+++ Exercise/6_5_rivers.py
@@ -0,0 +1,26 @@
+rivers = {
+    "Nile": ["Egypt", "Sudan", "South Sudan", "Ethiopia", "Uganda", "Democratic Republic of the Congo", "Kenya", "Tanzania", "Rwanda", "Burundi"],
+    "Amazon": ["Brazil", "Peru", "Colombia", "Bolivia", "Ecuador", "Venezuela", "Guyana"],
+    "Yangtze": ["China"],
+    "Mississippi": ["United States"],
+    "Danube": ["Germany", "Austria", "Slovakia", "Hungary", "Croatia", "Serbia", "Romania", "Bulgaria", "Moldova", "Ukraine"],
+    "Ganges": ["India", "Bangladesh"],
+    "Volga": ["Russia"],
+    "Mekong": ["China", "Myanmar", "Laos", "Thailand", "Cambodia", "Vietnam"],
+    "Indus": ["Pakistan", "India", "China"],
+    "Tigris": ["Turkey", "Iraq"],
+    "Euphrates": ["Turkey", "Syria", "Iraq"],
+    "Seine": ["France"],
+    "Thames": ["United Kingdom"],
+    "Zambezi": ["Zambia", "Angola", "Namibia", "Botswana", "Zimbabwe", "Mozambique"],
+    "Rhine": ["Switzerland", "Liechtenstein", "Austria", "Germany", "France", "Netherlands"],
+    "Loire": ["France"],
+    "Congo": ["Democratic Republic of the Congo", "Republic of the Congo", "Angola"],
+    "Niger": ["Guinea", "Mali", "Niger", "Nigeria"],
+    "Murray": ["Australia"],
+    "Po": ["Italy"]
+}
+for key , value in rivers.items():
+    print(f'{key} this are the rives: {value}')
+    print(f'{key}')
+    print(f"{value}")
