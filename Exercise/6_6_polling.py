2416d4bb805cbe2f5c13c316f4d72d1592db2180
Nagu <kondanagendher8@gmail.com>
2025-07-01 10:44:52 +0530

    setting right

--- Exercise/6_6_polling.py
+++ Exercise/6_6_polling.py
@@ -1,1 +1,14 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+    }
 
+list=['jen','sarah','mouli','vikas']
+keys = [value for value in favorite_languages.keys()]
+for ppl in list:
+    if ppl in keys:
+        print(f'thank for pooling {ppl}')
+    else:
+        print(f'u are welcome for pooling {ppl}')
