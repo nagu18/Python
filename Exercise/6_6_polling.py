874fc39e06e37ba750fcef0f6eaeff82d7082fb9
Nagu <kondanagendher8@gmail.com>
2025-07-01 10:46:07 +0530

    update

--- Exercise/6_6_polling.py
+++ Exercise/6_6_polling.py
@@ -6,7 +6,7 @@ favorite_languages = {
     }
 
 list=['jen','sarah','mouli','vikas']
-keys = [value for value in favorite_languages.keys()]
+keys = [value for value in favorite_languages.keys()] #comprahensive will work not list(favorite_languages.keys()) 
 for ppl in list:
     if ppl in keys:
         print(f'thank for pooling {ppl}')
