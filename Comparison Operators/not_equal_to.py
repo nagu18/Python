ef0ea6267ef552c081560e45405eefb2ca170975
Nagu <kondanagendher8@gmail.com>
2025-09-18 07:54:24 +0530

    update

--- Comparison Operators/not_equal_to.py
+++ Comparison Operators/not_equal_to.py
@@ -1,6 +1,13 @@
-not_accecpting='1000rs'
+'''not_accecpting='1000rs'
 cash = input()
 if cash != not_accecpting:  
     print('we accept this cash')
 else:
     print('we dont accpet 1000rs')
+'''
+no=1000
+cash = input()
+if cash != 1000:
+    print("we accept cash")
+else:
+    print("nope no change") 
--- comprahensive/even_range.py
+++ comprahensive/even_range.py
@@ -1,4 +1,21 @@
-list=[x for x in range(1,10) if x%2 == 0]
+'''list=[x for x in range(1,10) if x%2 == 0]
 print(list)
 # [expression for item in iterable if condition]
 #printing even num 
+'''
+list=[x for x in range(1,11)]
+print(list)
+
+
+arr=[]
+def even(x):
+    arr.append(x)
+    print(arr)
+flag = True
+while flag:
+    x=int(input())
+    if x%2==0:
+        even(x)
+    else:
+        print(arr)
+        flag=False
